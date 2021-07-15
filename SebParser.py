b3dcb043923afa5769723af92009733546411ba8
Vet <vd188@yandex.ru>
2021-07-15 18:02:46 +0300

    Add SebParser.py

---
+++ SebParser.py
@@ -0,0 +1,90 @@
+import re
+
+def is_isin_code(isisin):
+    pattern = re.compile("([A-Z]{2})((?![A-Z]{10})[A-Z0-9]{10})")
+    return (isinstance(isisin, str) and len(isisin) == 12 and bool(pattern.search(isisin)))
+
+
+class SebParser(BaseParser):
+
+    def __init__(self, file_path):
+        '''
+        :param file_path: Path to mutual fund report file
+        in this method we handle all rows in DataFrame using chain of responsibility.
+        You can similarly define your conditions as handler classes in heirs of BaseParser class.
+        This method must create:
+        - DataFrame with parsed data in self.parsed_data,
+        - datetime of report in self.date
+        - name of mutual fund in self.name
+        IMPORTANT: condition in Handlers must not intersect
+        '''
+
+        self.df = pd.read_csv(file_path, delimiter=';', header=None)
+
+        # create of chain of responsibility
+
+        self.handlers = self.HandleHeaderAndStartBody(
+            self.HandleEndBody(
+                self.HandleNameAndDate(
+                    self.NullHandler())))
+
+        self.start_index = self.end_index = self.columns = None
+
+        for index, row in self.df.iterrows():
+            self.handlers.handle(self, row, index)
+
+    class NullHandler:
+        '''
+        Null hanhdler for chain of responsibility implementation
+        '''
+
+        def __init__(self, successor=None):
+            self.__successor = successor
+
+        def handle(self, parser, row, index):
+            if self.__successor is not None:
+                self.__successor.handle(parser, row, index)
+
+    class HandleNameAndDate(NullHandler):
+        '''
+        Find and initialize date and name of mutual fund
+        '''
+
+        def handle(self, parser, row, index):
+            if (row[0]) == 'SubFund LEI' and (index + 1) < parser.df.shape[0] and \
+                    (parser.df.iloc[index + 1, 0]) == 'Date' and index > 0:
+                parser.name = parser.df.iloc[index - 1, 0]
+                parser.date = datetime.strptime(parser.df.iloc[index + 1, 1], '%Y-%m-%d')
+            else:
+                super().handle(parser, row, index)
+
+    class HandleHeaderAndStartBody(NullHandler):
+        '''
+        Find and initialize header and start index of "body"
+        '''
+
+        def handle(self, parser, row, index):
+            if (row[0]) == 'ISIN' and (row[1]) == 'Security Name':
+
+                parser.start_index = (index + 1, 0)
+                parser.columns = parser.df.iloc[index: index + 1, :].values.flatten().tolist()
+                parser.columns[1] = 'Name'
+                parser.columns[19] = 'Weight'
+                parser.columns[0] = 'Code'
+            else:
+                super().handle(parser, row, index)
+
+    class HandleEndBody(NullHandler):
+        '''
+        Find end index of "body"
+        '''
+
+        def handle(self, parser, row, index):
+            if is_isin_code(row[0]) and index == parser.df.shape[0] - 1:
+                parser.end_index = (index, 0)
+                parser.parsed_data = parser.df.iloc[parser.start_index[0]:parser.end_index[0], :].dropna(how='all')
+                parser.parsed_data.columns = parser.columns
+                parser.parsed_data = parser.parsed_data.reset_index(drop=True)
+                parser.parsed_data.Weight = parser.parsed_data.Weight.map(lambda x: float(x) * 100)
+            else:
+                super().handle(parser, row, index)
